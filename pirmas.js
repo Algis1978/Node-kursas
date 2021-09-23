fbb00b1b5326efd9d34d40a681e8d149c7ddabb2
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-09-23 11:57:39 +0300

    add nodeKursas4

--- pirmas.js
+++ pirmas.js
@@ -7,3 +7,12 @@ console.log(path.extname(byla));
 const mat = require("./mat.js");
 console.log(mat.sudetis(6, 4));
 console.log(mat.atimtis(6, 4));
+
+function atsitiktinisSkaicius ( max = 100 ) 
+{ return Math.ceil( Math.random()*max );}
+console.log(atsitiktinisSkaicius());
+console.log(process.argv);
+
+const [ pirmas, antras, vartotojoSkaicius ] = process.argv;
+console.log( atsitiktinisSkaicius(vartotojoSkaicius) );
+
